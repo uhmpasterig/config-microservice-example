1b021d28797d0596b097655b6999d55a7c6024ef
Uhmpasterig <805luca805@gmail.com>
2024-04-06 13:37:27 +0200

    fix: remove env variables for message patterns

--- src/controllers/config.controller.ts
+++ src/controllers/config.controller.ts
@@ -14,25 +14,24 @@ export class ConfigController {
 
   constructor(private readonly redisService: RedisService) {}
 
-  @EventPattern(process.env.SET_PATTERN || 'set')
-  async set(
-    @Payload() data: { key: string; value: any },
-    @Ctx() context: RmqContext,
-  ) {
+  @MessagePattern({ cmd: 'set' })
+  async set(@Payload() data: { key: string; value: any }) {
     try {
       await this.redisService.set(data.key, data.value);
     } catch (error) {
       this.logger.error(`Error while setting key ${data.key}:`, error.stack);
+      throw error;
     }
   }
 
-  @MessagePattern({ cmd: process.env.GET_PATTERN || 'get' })
-  async get(key: string): Promise<any> {
+  @MessagePattern({ cmd: 'get' })
+  async get(@Payload() key: string): Promise<any> {
     try {
       const value = await this.redisService.get(key);
       return value ?? null;
     } catch (error) {
       this.logger.error(`Error while getting key ${key}:`, error.stack);
+      throw error;
     }
   }
 }
